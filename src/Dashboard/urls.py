e906ab6f6feb22e3f31652d6acda308137107a2e
lotfi165 <51199772+lotfi165@users.noreply.github.com>
2024-01-19 07:16:07 +0100

    sign in sign out

--- src/Dashboard/urls.py
+++ src/Dashboard/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.index),
-  path('sign-in/', views.signIn),
+  path('', views.index, name='index'),
+  path('sign-in/', views.signIn, name='sign-in'),
+  path('sign-out/', views.signOut, name='sign-out'),
+  path('dashboard/', views.dashboard, name='dashboard'),
 ]
--- src/Dashboard/views.py
+++ src/Dashboard/views.py
@@ -1,9 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .decorators import login_not_required
 
 # Create your views here.
+@login_not_required
 def index(request: HttpRequest):
-  return render(request, 'index.html')
+  return render(request, 'pages/index.html')
 
+@login_required(login_url='sign-in')
+def dashboard(request: HttpRequest):
+  return render(request, 'pages/dashboard-home.html')
+
+@login_not_required
 def signIn(request: HttpRequest):
-  return render(request, 'sign-in.html')
+  if request.method == 'POST':
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request=request, username=username, password=password);
+    if user is not None:
+      login(request=request, user=user)
+      return redirect('dashboard');
+    else:
+      messages.error(request=request, message="Wrong username or password");
+
+  return render(request, 'pages/sign-in.html')
+
+@login_required(login_url='sign-in')
+def signOut(request: HttpRequest):
+  logout(request=request)
+  return redirect('sign-in');
