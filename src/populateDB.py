100a1d087f6cd92fe61ce47beea74770caf585ad
lotfi165 <51199772+lotfi165@users.noreply.github.com>
2024-01-28 23:24:15 +0100

    update

--- src/populateDB.py
+++
@@ -1,6 +0,0 @@
-from Dashboard.models import Department, Appointment, AppointmentState, Genders, MedicalHistory, MedicalStaff, Patient, Prescription, Procedure, ProcedureApplication, ProcedureType, Speciality
-import django
-
-django.setup()
-
-Department.objects.create(name='Cardiology')
